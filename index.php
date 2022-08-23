e5f705f1ba6fc379e67ce5ab8a399cd446820bf2
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2022-08-23 14:52:26 +0300

    include a partials folder

--- index.php
+++ index.php
@@ -10,24 +10,7 @@
 </head>
 
 <body>
-    <!-- Nav -->
-    <header class="text-gray-600 body-font shadow-sm">
-        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-            <a class="title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                <span class="ml-3 text-xl">Cart Template</span>
-            </a>
-            <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                <a class="mr-5 hover:text-gray-900">Home</a>
-                <a class="mr-5 hover:text-gray-900">Products</a>
-                <a class="mr-5 hover:text-gray-900">Cart</a>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-            </nav>
-        </div>
-    </header>
+    <?php include_once 'includes/nav.php';?>
 
     <!-- Hero -->
     <section class="text-gray-600 body-font shadow-sm">
@@ -51,23 +34,7 @@
         </div>
     </section>
 
-    <!-- Footer -->
-    <footer class="text-gray-600 body-font ">
-        <div class="container px-5 pt-4 mx-auto flex items-center sm:flex-row flex-col">
-            <a class="title-font font-medium items-center md:justify-start justify-center text-gray-900">
-
-                <span class="ml-3 text-xl">Cart Template</span>
-            </a>
-            <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">©
-                2020 Cart Template
-            </p>
-            <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                <a class="mr-5 hover:text-gray-900">Home</a>
-                <a class="mr-5 hover:text-gray-900">Products</a>
-                <a class="mr-5 hover:text-gray-900">Cart</a>
-            </span>
-        </div>
-    </footer>
+    <?php include_once 'includes/footer.php';?>
 </body>
 
 </html>
